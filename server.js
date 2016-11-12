c09fbcb00ea98fa59576300455adb9a231ec4d58
Kinokus <aleks.kinokus@gmail.com>
2016-11-12 14:05:06 +0200

    adding server side

---
+++ server.js
@@ -0,0 +1,170 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var http = require("http");
+const url = require('url');
+var mysql = require('mysql');
+var app = express();
+var port = 3000;
+
+var dataBaseScript = "" +
+    "CREATE TABLE IF NOT EXISTS " +
+    "`events`.`users` " +
+    "(" +
+    "`userId` INT UNSIGNED AUTO_INCREMENT ," +
+    "`name` VARCHAR(32)  DEFAULT NULL ," +
+    "`password` VARCHAR(32)  DEFAULT NULL ," +
+    "`shown_name` VARCHAR(32)  DEFAULT NULL ," +
+    "`token` VARCHAR(64)  DEFAULT NULL ," +
+    "PRIMARY KEY (`userId`), " +
+    "UNIQUE `name` (`name`)" +
+    ");";
+
+var tableStudentScript = "" +
+    "CREATE TABLE TABLE IF NOT EXISTS `students` (    " +
+    "`idStudent` int(11) NOT NULL,    " +
+    "`firstName` varchar(35) DEFAULT NULL,    " +
+    "`lastName` varchar(50) DEFAULT NULL,    " +
+    "`phone` varchar(20) DEFAULT NULL,    " +
+    "`e-mail` varchar(255) DEFAULT NULL,    " +
+    "`address` varchar(255) DEFAULT NULL,    " +
+    "`passportID` int(11) DEFAULT NULL" +
+    ")";
+{
+    var insertScript = "INSERT INTO `users` " +
+        "(" +
+        "`userId`, " +
+        "`name`, " +
+        "`password`, " +
+        "`shown_name`, " +
+        "`token`" +
+        ") " +
+        "VALUES (" +
+        "NULL, " +
+        "'_NAME_', " +
+        "'_PASS_', " +
+        "NULL, " +
+        "NULL" +
+        ");";
+}
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'events'
+});
+
+connection.query(dataBaseScript, function (err, res, fields) {
+    console.log(err);
+});
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.listen(port, function () {
+    console.log("Started on PORT " + port);
+});
+
+// app.get('/registerNamePassword', function (getReq, getRes) {});
+
+app.get('/*', function (getReq, getRes) {
+    switch (getReq.path) {
+        case '/registerNamePassword':
+        {
+            getReq.on('data', function () {
+            });
+            getReq.on('end', function () {
+                // console.log(req.query);
+                name = getReq.query.name;
+                pass = getReq.query.pass;
+
+                var sqlScript = 'insert into users set ?';
+                connection.query(sqlScript, {name: name, password: pass}, function (err, sqlRes) {
+                    var result = {};
+                    if (err) {
+                        console.log(err);
+                        result.status = 'error';
+                        result.reason = 'insert name-password pair failed';
+                    } else {
+                        console.log(sqlRes);
+                        result.status = 'ok';
+                        result.reason = 'new record inserted';
+
+                    }
+                    getRes.end(JSON.stringify(result));
+                });
+            });
+            break;
+        }
+        case '/addUser':
+        {
+            getReq.on('data', function () {});
+            getReq.on('end', function () {
+                console.log(req.query);
+                firstName = getReq.query.firstName;
+                lastName = getReq.query.lastName;
+                passportID = getReq.query.passportID;
+                var sqlScript = 'insert into students set ?';
+                var studentSet = {
+                    firstName: firstName,
+                    lastName: lastName,
+                    passportID: passportID
+                };
+                connection.query(sqlScript, studentSet, function (err, sqlRes) {
+                    var result = {};
+                    if (err) {
+                        console.log(err);
+                        result.status = 'error';
+                        result.reason = 'insert failed';
+                        result.fullErrorText = err;
+                    } else {
+                        console.log(sqlRes);
+                        result.status = 'ok';
+                        result.reason = 'new record inserted';
+                    }
+                    getRes.end(JSON.stringify(result));
+                });
+            });
+            break;
+        }
+        case '/sendNamePassword':
+        {
+            getReq.on('data', function () {
+                // console.log(e);
+            });
+
+            getReq.on('end', function () {
+                // console.log(req.query);
+                name = getReq.query.name;
+                pass = getReq.query.pass;
+
+                var sqlScript = 'select password from users where ?';
+                connection.query(sqlScript, {name: name}, function (err, sqlRes) {
+                    var result = {};
+                    if (sqlRes.length == 0) {
+                        result.status = 'error';
+                        result.reason = 'userNotFound';
+                    } else if (pass == sqlRes[0].password) {
+                        result.status = 'ok';
+                        result.reason = 'passwordConfirmed';
+                    } else {
+                        result.status = 'error';
+                        result.reason = 'userAndPasswordNotMatches';
+                    }
+                    getRes.end(JSON.stringify(result));
+                });
+            });
+            break;
+        }
+        case '/':
+        {
+            getRes.sendfile("index.html");
+            break
+        }
+        default:
+        {
+            getRes.sendfile(getReq.path);
+        }
+    }
+});
+
+
