d74fe75ef2f8a02a93460f936e13fb18546539aa
Kinokus <aleks.kinokus@gmail.com>
2016-11-13 10:13:54 +0200

    "update server, add favicon"

--- server.js
+++ server.js
@@ -5,39 +5,40 @@ const url = require('url');
 var mysql = require('mysql');
 var app = express();
 var port = 3000;
+var dataBaseName = 'learning sheduler';
 
-var tableStudentScript = "" +
-    "CREATE TABLE IF NOT EXISTS `students` (    " +
-    "`idStudent` int(11) UNSIGNED AUTO_INCREMENT ,    " +
-    "`firstName` varchar(35) DEFAULT NULL,    " +
-    "`lastName` varchar(50) DEFAULT NULL,    " +
-    "`phone` varchar(20) DEFAULT NULL,    " +
-    "`e-mail` varchar(255) DEFAULT NULL,    " +
-    "`address` varchar(255) DEFAULT NULL,    " +
-    "`passportID` int(11) DEFAULT NULL," +
-    "PRIMARY KEY (`idStudent`)," +
-    "UNIQUE `passportID`(`passportID`)" +
-    ")";
-
+{
+    var tableStudentScript = "" +
+        "CREATE TABLE IF NOT EXISTS `" +
+        'students' +
+        "` (    " +
+        "`idStudent` int(11) UNSIGNED AUTO_INCREMENT ,    " +
+        "`firstName` varchar(35) DEFAULT NULL,    " +
+        "`lastName` varchar(50) DEFAULT NULL,    " +
+        "`phone` varchar(20) DEFAULT NULL,    " +
+        "`e-mail` varchar(255) DEFAULT NULL,    " +
+        "`address` varchar(255) DEFAULT NULL,    " +
+        "`passportID` int(11) DEFAULT NULL," +
+        "PRIMARY KEY (`idStudent`)," +
+        "UNIQUE `passportID`(`passportID`)" +
+        ")";
+}
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'learning sheduler'
+    port:3306,
+    database: dataBaseName
 });
 
 connection.query(tableStudentScript, function (err, res, fields) {
     console.log(err);
 });
-
-
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.listen(port, function () {
     console.log("Started on PORT " + port);
 });
-
-// app.get('/registerNamePassword', function (getReq, getRes) {});
 
 app.get('/*', function (getReq, getRes) {
     switch (getReq.path) {
