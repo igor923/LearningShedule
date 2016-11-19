759cc062c11acd9ec3805f5fdd10b2a289f0672e
Kinokus <aleks.kinokus@gmail.com>
2016-11-19 13:53:43 +0200

    "added set token, added get schedule, added check token"

--- sqlCreators.js
+++ sqlCreators.js
@@ -1,3 +1,9 @@
 /**
  * Created by dev on 18.11.2016.
  */
+module.exports.sqlCreators = function () {
+    return {
+        tableAuthScript:
+            "CREATE TABLE IF NOT EXISTS `events`.`auth` ( `idAuth` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(25) NOT NULL , `pass` VARCHAR(45) NOT NULL , PRIMARY KEY (`idAuth`)) ENGINE = MyISAM;"
+    }
+};
